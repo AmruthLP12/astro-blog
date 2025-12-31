f5b51660d4d9c714d0072e8d8980b36d824935e7
Amruth <amruthlp12@gmail.com>
2025-12-31 15:37:05 +0530

    feat(blog): update site URL and SEO metadata in BaseLayout

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,24 +3,29 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-    resolve: {
-      alias: {
-        "@": "src",
-      },
+  vite: {
+  resolve: {
+    alias: {
+      "@": "src",
     },
-
-    plugins: [tailwindcss()],
   },
 
+  plugins: [tailwindcss()],
+},
+
   output: "static",
+
   markdown: {
     shikiConfig: {
       theme: "one-dark-pro",
       wrap: true,
     },
   },
-  site: "http://localhost:4321",
+
+  site: "https://tecnodrishti.vercel.app",
+  integrations: [sitemap()],
 });
